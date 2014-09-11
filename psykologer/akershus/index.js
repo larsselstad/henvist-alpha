48aa43bfd877319654f10f2c30a9cbcf598a052e
Lars Selstad <lars@lase.no>
2014-09-11 08:09:22 +0200

    Fjernet corrections i akershus

--- psykologer/akershus/index.js
+++ psykologer/akershus/index.js
@@ -9,24 +9,7 @@ var fs = require('fs'),
     pageUtil = require(root + '/src/pageUtil'),
     htmlMaker = require(root + '/src/htmlMaker');
 
-var corrections = {
-    name: {
-        'nicke, Erik': 'Stänicke, Erik'
-    },
-    phone: {
-        '2424270': '22424270',
-        '2430824': '22430824',
-        '2468163': '22468163',
-        '2600200': '22600200',
-        '2845125': '22845125',
-        '2563567': '22563567',
-        '2581783': '22581783',
-        '2923759': '22923759',
-        '2491197': '22491197',
-        '689804': '21689804',
-        '8174959': '48174959'
-    }
-};
+var corrections = {};
 
 var pathToPdf = __dirname + "/Psykologer_Akershus_telefonliste.pdf";
 
