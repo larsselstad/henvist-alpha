7f85eec62abc790e4128e05a7fa29dd3815689e5
Lars Selstad <lars@lase.no>
2014-10-31 20:04:41 +0100

    Oppdatert koden for nevropsykologer i Oslo

--- nevropsykologer/oslo/index.js
+++ nevropsykologer/oslo/index.js
@@ -1,13 +1,4 @@
-// Script for å lage psykologer_oslo.html
-
 /*jshint node: true*/
-
-var root = process.cwd();
-
-var fs = require('fs'),
-    PDFParser = require("pdf2json/pdfparser"),
-    pageUtil = require(root + '/src/pageUtil'),
-    htmlMaker = require(root + '/src/htmlMaker');
 
 var corrections = {
     phone: {
@@ -16,35 +7,21 @@ var corrections = {
     }
 };
 
+/*jshint node: true*/
+
+var root = process.cwd();
+
+var pdfToHtml = require(root + '/src/pdfToHtml');
+
 var pathToPdf = __dirname + "/Psykologer_Oslo_telefonliste.pdf";
 
-var pdfParser = new PDFParser();
-
-// denne bør bare returnere grid
-// løses med ett async/promise biblotek
-pdfParser.on("pdfParser_dataReady", function (pdfData) {
-    console.log('This file is dead');
-
-    /*var grid = [];
-
-    pdfData.data.Pages.forEach(function (page) {
-        page.Texts.forEach(pageUtil.extractPage(grid));
-    });
-
-    var pagesData = pageUtil.extractData(grid, {
+pdfToHtml({
+    categories: {
         'Psykologer:': 'Psykologer:',
         'Nevropsykologer:': 'Nevropsykologer:'
-    }, corrections);
-
-    htmlMaker(root, __dirname, 'Nevropsykologer', pagesData['Nevropsykologer:']);*/
-});
-
-pdfParser.on("pdfParser_dataError", function (data) {
-    console.log('error!');
-});
-
-fs.readFile(pathToPdf, function (err, pdfBuffer) {
-    if (!err) {
-        pdfParser.parseBuffer(pdfBuffer);
-    }
+    },
+    dir: __dirname,
+    pageTitle: 'Nevropsykologer i Oslo',
+    pagesDataKey: 'Nevropsykologer:',
+    pathToPdf: pathToPdf
 });
