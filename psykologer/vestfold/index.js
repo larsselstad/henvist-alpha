d4c711d2ea45e21ff72d74c41a16ddef7924ad68
Lars Selstad <lars@lase.no>
2015-04-13 08:42:06 +0200

    Lagt til script for Vestfold

---
+++ psykologer/vestfold/index.js
@@ -0,0 +1,19 @@
+/*jshint node: true*/
+
+var root = process.cwd();
+
+var pdfToHtml = require(root + '/src/pdfToHtml');
+
+var pathToPdf = root + "/data/Psykologer Vestfold telefonliste.pdf";
+
+pdfToHtml({
+    categories: {
+        'Psykologer:': 'Psykologer:',
+        'Nevropsykologer:': 'Nevropsykologer:'
+    },
+    dir: __dirname,
+    pageTitle: 'Psykologer i Vestfold',
+    pagesDataKey: 'Psykologer:',
+    pathToPdf: pathToPdf,
+    headerIndex: 1
+});
