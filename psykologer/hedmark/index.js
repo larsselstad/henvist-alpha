efae2c836879a1571f65b2586055e36905e2289a
Lars Selstad <lars@lase.no>
2015-04-13 08:35:57 +0200

    Lagt til script for Hedmark

---
+++ psykologer/hedmark/index.js
@@ -0,0 +1,19 @@
+/*jshint node: true*/
+
+var root = process.cwd();
+
+var pdfToHtml = require(root + '/src/pdfToHtml');
+
+var pathToPdf = root + "/data/Psykologer Hedmark telefonliste.pdf";
+
+pdfToHtml({
+    categories: {
+        'Psykologer:': 'Psykologer:',
+        'Nevropsykologer:': 'Nevropsykologer:'
+    },
+    dir: __dirname,
+    pageTitle: 'Psykologer i Hedmark',
+    pagesDataKey: 'Psykologer:',
+    pathToPdf: pathToPdf,
+    headerIndex: 1
+});
