290f1fb18df89db7cea3d558a162ce93a66b4333
Lars Selstad <lars@lase.no>
2015-04-13 08:38:28 +0200

    Lagt til script for Oppland

---
+++ psykologer/oppland/index.js
@@ -0,0 +1,19 @@
+/*jshint node: true*/
+
+var root = process.cwd();
+
+var pdfToHtml = require(root + '/src/pdfToHtml');
+
+var pathToPdf = root + "/data/Psykologer Oppland telefonliste.pdf";
+
+pdfToHtml({
+    categories: {
+        'Psykologer:': 'Psykologer:',
+        'Nevropsykologer:': 'Nevropsykologer:'
+    },
+    dir: __dirname,
+    pageTitle: 'Psykologer i Oppland',
+    pagesDataKey: 'Psykologer:',
+    pathToPdf: pathToPdf,
+    headerIndex: 1
+});
