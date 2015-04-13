ab04da5d2c77bb4067870b242b7d144af7427a21
Lars Selstad <lars@lase.no>
2015-04-13 08:40:54 +0200

    Lagt til script for Vest-Agder

---
+++ psykologer/vest-agder/index.js
@@ -0,0 +1,19 @@
+/*jshint node: true*/
+
+var root = process.cwd();
+
+var pdfToHtml = require(root + '/src/pdfToHtml');
+
+var pathToPdf = root + "/data/Psykologer Vest-Agder telefonliste.pdf";
+
+pdfToHtml({
+    categories: {
+        'Psykologer:': 'Psykologer:',
+        'Nevropsykologer:': 'Nevropsykologer:'
+    },
+    dir: __dirname,
+    pageTitle: 'Psykologer i Vest-Agder',
+    pagesDataKey: 'Psykologer:',
+    pathToPdf: pathToPdf,
+    headerIndex: 1
+});
